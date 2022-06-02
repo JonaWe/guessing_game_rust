914a9d3f0749b9fa7d30e5d19a3e204482d22dcb
Jona Wessendorf <jonawessendorf@gmail.com>
2022-06-02 22:02:37 +0200

    adds error handling

--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,11 @@ fn main() {
             .read_line(&mut guess)
             .expect("failed to read line");
 
-        let guess: u32 = guess
-            .trim()
-            .parse()
-            .expect("Please enter a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
 
         println!("You guessed: {}", guess);
 
