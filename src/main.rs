ca4acb97854aadefa4500034c733b8da9b7834e8
Jona Wessendorf <jonawessendorf@gmail.com>
2022-06-02 22:21:51 +0200

    adds difficulty

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,40 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
-fn main() {
-    println!("Guess the random number in the range [1,100]!");
+fn get_upper_bound() -> u32 {
+    println!("Choose between 3 difficulties:");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("\te = easy [1,10]");
+    println!("\tm = medium [1,100]");
+    println!("\th = hard [1,1000]");
+
+    loop {
+        let mut difficulty = String::new();
+
+        io::stdin()
+            .read_line(&mut difficulty)
+            .expect("failed to read line");
+
+        let upper_bound: u32 = match difficulty.trim() {
+            "e" => 10,
+            "m" => 100,
+            "h" => 1000,
+            _ => {
+                println!("invalid input (choose e, m or h)");
+                continue;
+            },
+        };
+
+        return upper_bound;
+    }
+}
+
+fn main() {
+    let upper_bound = get_upper_bound();
+
+    println!("Guess the random number in the range [1,{}]!", upper_bound);
+
+    let secret_number = rand::thread_rng().gen_range(1..=upper_bound);
 
     loop {
         println!("Please input your guess (q for quit).");
